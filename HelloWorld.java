b37a8ea411a4de10a128529bf4702c43c2b60623
ankurmalviyademo <61942237+ankurmalviyademo@users.noreply.github.com>
2020-03-11 06:44:31 +0530

    updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,10 @@
 public class HelloWorld {
 	public static void main (String[]args) {
 		System.out.println("Hello World");
+		System.out.println("Hello India1");
+				System.out.println("Hello World2");
+		System.out.println("Hello India");
+				System.out.println("Hello World3");
 		System.out.println("Hello India");
 	}
 }
